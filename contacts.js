4bf8dae4d1c668313a860974dc2f5e45a4c551ac
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-20 22:43:47 +0100

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,2 +1,8 @@
 const fs = require('fs/promises');
 const path = require('path');
+
+// Складаємо шлях до файлу contacts.json
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+// Виводимо шлях до файлу contacts.json
+console.log(contactsPath);
