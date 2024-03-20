890ca14c3fff3f9e358ae84bbbf6feb865d3869e
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-20 23:29:39 +0100

    listContacts

--- contacts.js
+++ contacts.js
@@ -1,8 +1,41 @@
 const fs = require('fs/promises');
 const path = require('path');
-
-// Складаємо шлях до файлу contacts.json
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
-// Виводимо шлях до файлу contacts.json
-console.log(contactsPath);
+
+// Повертає масив контактів.
+async function listContacts() {
+    try {
+        const data = await fs.readFile(contactsPath);
+        return JSON.parse(data);
+    } catch (error) {
+        return [];
+    }
+}
+
+
+async function getContactById(contactId) {
+    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+    // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+
+
+
+
+
+
+
+
+// listContacts().then(contacts => {
+//     console.log(contacts);
+// }).catch(error => {
+//     console.error('Ошибка при чтении контактов:', error);
+// });
